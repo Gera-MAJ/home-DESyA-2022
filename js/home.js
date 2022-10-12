9aa32b95b821dfa5728840caa18ecec8843f74dc
Gera-MAJ <jatipg@gmail.com>
2022-10-11 23:58:00 -0300

    reducido el codigo de js

--- js/home.js
+++ js/home.js
@@ -1,7 +1,6 @@
 
 const columnaDerecha = document.querySelector('.columnaDerecha');
-// const fondoMenu =  document.getElementById('fondoMenu');
-// const iconoMenu = document.getElementById('iconoMenu')
+
 
 document.getElementById('menu_contenedor').addEventListener('click', activarMenu)
 
@@ -16,28 +15,3 @@ function activarMenu(){
 
     columnaDerecha.classList.toggle("activar_columnaDerecha")
 }
-
-
-// iconoMenu.addEventListener('click', aparecerMenu)
-//El stopPropagation, como el columna derecha es hijo del fondo menu, este le afectaba todo. Con el stoppropagation, detengo esa afectacion. 
-// columnaDerecha.addEventListener('click', e => {e.stopPropagation()})
-
-// fondoMenu.addEventListener('click', (e) => {
-    
-//     fondoMenu.style.display = "none";
-//     columnaDerecha.style.right = "-1000px";
-//     iconoMenu.style.visibility = "visible";
-// })
-
-
-// function aparecerMenu(){
-//     columnaDerecha.style.right = "-178px";
-//     iconoMenu.style.visibility = "hidden"
-//     fondoMenu.style.display = "block" 
-// }
-
-// function desaparecerMenu(){
-//     fondoMenu.style.display = "none";
-//     columnaDerecha.style.right = "-1000px";
-//     iconoMenu.style.display = "block";
-// }    
