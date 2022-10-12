ac48aaf09247bc5c09ee01ca87a363bd0b9a53bb
Gera-MAJ <jatipg@gmail.com>
2022-10-12 00:05:23 -0300

    cambiando color del modo salir del menu

--- js/home.js
+++ js/home.js
@@ -1,8 +1,8 @@
 
 const columnaDerecha = document.querySelector('.columnaDerecha');
+const menu_contenedor = document.querySelector('.menu_contenedor')
 
-
-document.getElementById('menu_contenedor').addEventListener('click', activarMenu)
+document.querySelector('.menu_contenedor').addEventListener('click', activarMenu)
 
 const line1 = document.querySelector(".line1_bars");
 const line2 = document.querySelector(".line2_bars");
@@ -13,5 +13,6 @@ function activarMenu(){
     line2.classList.toggle("active_line2_bars")
     line3.classList.toggle("active_line3_bars")
 
+    menu_contenedor.classList.toggle('activar_menu_contenedor')
     columnaDerecha.classList.toggle("activar_columnaDerecha")
 }
