87982277ceeb9aeebd7c80062057a62412e4729d
Gera-MAJ <jatipg@gmail.com>
2022-10-31 12:53:31 -0300

    colocando un menú movil que funcione bien se haga scroll

--- js/home.js
+++ js/home.js
@@ -1,12 +1,16 @@
 
+
 const columnaDerecha = document.querySelector('.columnaDerecha');
 const menu_contenedor = document.querySelector('.menu_contenedor')
 
 document.querySelector('.menu_contenedor').addEventListener('click', activarMenu)
+//con esta forma puedo hacer que cambie el menu de arriba en la vista movil
+window.addEventListener('scroll', activarMenuMovil)
 
 const line1 = document.querySelector(".line1_bars");
 const line2 = document.querySelector(".line2_bars");
 const line3 = document.querySelector(".line3_bars");
+const barra = document.querySelector(".menu_movil")
 
 function activarMenu(){
     line1.classList.toggle("active_line1_bars")
@@ -15,4 +19,8 @@ function activarMenu(){
 
     menu_contenedor.classList.toggle('activar_menu_contenedor')
     columnaDerecha.classList.toggle("activar_columnaDerecha")
+}
+
+function activarMenuMovil(){
+    barra.classList.toggle("active_menu_movil", window.scrollY>0)
 }
